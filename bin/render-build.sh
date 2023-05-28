99ae8e1f99cad3c1a5feba1bf232ecb5f6a3e4f0
Ritesh Kapoor <ritesh@workoidz.com>
2023-05-28 13:00:24 +0530

    Gemfile lock updated

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # the current version of Nokogiri ships with incompatible libs
 bundle config --local build.nokogiri --use-system-libraries
 
-bundle lock --add-platform x86_64-linux
+
 
 bundle install
 bundle exec rake assets:precompile
